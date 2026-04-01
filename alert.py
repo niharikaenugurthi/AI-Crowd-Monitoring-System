69fff60e8fd95c72d1036b01c52cb4581e4e3039
Niharika875 <niharikaenugurthi@gmail.com>
2026-04-01 23:16:05 +0530

    fixed aws issue

--- alert.py
+++ alert.py
@@ -1,17 +1,2 @@
-import boto3
-
-sns = boto3.client('sns')
-
-TOPIC_ARN = "arn:aws:sns:ap-south-1:668226797255:crowd-alert-system"
-
 def send_alert(count):
-    
-    message = f"⚠ Crowd density HIGH. People detected: {count}"
-
-    sns.publish(
-        TopicArn=TOPIC_ARN,
-        Message=message,
-        Subject="Crowd Monitoring Alert"
-    )
-
-    print("Alert sent!")
+    print(f"⚠ Crowd Alert: {count} people detected")
--- aws_upload.py
+++ aws_upload.py
@@ -1,13 +1,2 @@
-import boto3
-
-# connect to AWS S3
-s3 = boto3.client('s3')
-
-bucket_name = "crowd-monitoring-project"
-
 def upload_image(file_name):
-    try:
-        s3.upload_file(file_name, bucket_name, file_name)
-        print("Image uploaded to S3")
-    except Exception as e:
-        print("Upload failed:", e)
+    print("Image upload skipped (AWS disabled)")
